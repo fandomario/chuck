556fb21835189904f6a900c55c5dea769655ce2f
fandomario <71139801+fandomario@users.noreply.github.com>
2020-10-25 01:54:50 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -10,20 +10,21 @@ first_options_list = "animal, career, celebrity, dev, explicit, fashion, food, h
 def hello_world():
     category = request.args.get('category')
     if not category:
-      return render_template('index.html', categories=first_options_list)
+        return render_template('index.j2', categories=first_options_list)
     options_list = requests.get('https://api.chucknorris.io/jokes/categories')
     options_list = options_list.json()
-    options_list =', '.join(options_list)
+    options_list = ', '.join(options_list)
     if category not in options_list:
-      return render_template('index.html', categories=options_list, wrong="Choose another")
+        return render_template('index.j2', categories=options_list, wrong="Choose another")
     the_joke = requests.get(f'https://api.chucknorris.io/jokes/random?category={category}').json()
     joke = the_joke['value']
     return render_template(
-      'index.html',
-      categories=options_list,
-      category=category,
-      joke=joke,
-      )
+        'index.j2',
+        categories=options_list,
+        category=category,
+        joke=joke,
+    )
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(threaded=True, port=5000)
